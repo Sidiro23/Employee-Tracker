ef7988c4adf9cfbbb1d5ba79ee48b6121b859deb
soutsos <106550353+Sidiro23@users.noreply.github.com>
2022-07-31 19:20:21 -0500

    started inquirer selections and functions for query

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const consoleTable = require('console.table');
 require('dotenv').config();
 
-
+//connect to db
 const db = mysql.createConnection(
   
     process.env.DB_NAME,
@@ -15,5 +15,73 @@ const db = mysql.createConnection(
       port: 3301
     
   },
-  console.log(`Connected to the employees_db database.`)
+  
 );
+connection.connect(function(err){
+  if (err) throw err;
+  selections();
+})
+console.log(`Connected to the employees_db database.`)
+//make selections with inquirer
+function selections(){
+  inquirer.prompt({
+    name:'selection',
+    type:'list',
+    message:'Welcome to our employee database.What would you like to do?',
+    choices:[
+      'View all employees',
+      'View all departments',
+      'View all roles',
+      'Add an employee',
+      'Add a department',
+      'Add a role',
+      'Update employee role',
+      'Delete an employee',
+      'Exit'
+    ]
+  }).then(function(reply){
+    switch (reply.action){
+      case 'View all employees':
+        viewEmployees();
+        break;
+      case 'View all departments':
+        viewDepartments();
+        break;
+      case 'View all roles':
+        viewRoles();
+        break;
+      case 'Add an employee':
+        addEmployee();
+        break;
+      case 'Add a department':
+        addDept();
+        break;
+      case 'Add a role':
+        addRole();
+        break;
+      case 'Update employee role':
+        updateRole();
+        break;
+      case 'Delete an employee':
+        delEmployee();
+        break;
+      case 'Exit':
+        exitApp();
+        break;
+      default:
+        break;                  
+    }
+  })
+};
+
+//function and quiry to view all employees in the database
+
+function viewEmployees(){
+  let query = 'SELECT * FROM employee';
+  connection.query(query, function(err,res){
+    if (err) throw err;
+    console.log(res.length + 'employees found!');
+    console.table('All Employees:', res);
+    selections();
+ })
+};
